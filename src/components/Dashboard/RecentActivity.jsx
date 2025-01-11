08c31fd50f2fc04afd78f2598d4bcde26a9c849e
Anjana V U <103420152+anjanavu@users.noreply.github.com>
2025-01-11 14:55:52 +0530

    #edit

--- src/components/Dashboard/RecentActivity.jsx
+++ src/components/Dashboard/RecentActivity.jsx
@@ -7,6 +7,7 @@ import {
   TableContainer,
   TableRow,
   Paper,
+  TableHead,
 } from "@mui/material";
 import moment from "moment";
 
@@ -30,6 +31,25 @@ const RecentActivity = ({ data }) => (
     </Typography>
     <TableContainer component={Paper} elevation={0}>
       <Table>
+      <TableHead>
+          <TableRow>
+            <TableCell sx={{ fontWeight: "bold", fontSize: "12px", color: "#4A4A4A" }}>
+              Admin Name
+            </TableCell>
+            <TableCell sx={{ fontWeight: "bold", fontSize: "12px", color: "#4A4A4A" }}>
+              Description
+            </TableCell>
+            <TableCell sx={{ fontWeight: "bold", fontSize: "12px", color: "#4A4A4A" }}>
+              Host
+            </TableCell>
+            <TableCell sx={{ fontWeight: "bold", fontSize: "12px", color: "#4A4A4A" }}>
+              Agent
+            </TableCell>
+            <TableCell sx={{ fontWeight: "bold", fontSize: "12px", color: "#4A4A4A" }}>
+              Date
+            </TableCell>
+          </TableRow>
+        </TableHead>
         {data?.length > 0 ? (
           <TableBody>
             {data?.map((row, index) => (
@@ -39,20 +59,24 @@ const RecentActivity = ({ data }) => (
                   "&:last-child td, &:last-child th": { border: 0 },
                 }}
               >
-                <TableCell sx={{ color: "#687784" }}>{row.adminName}</TableCell>
+                <TableCell sx={{ color: "#687784", fontSize: "10px" }}>
+                  {row.adminName}
+                </TableCell>
 
-                <TableCell sx={{ color: "#687784" }}>
+                <TableCell sx={{ color: "#687784", fontSize: "10px" }}>
                   {row.description}
                 </TableCell>
 
-                <TableCell sx={{ color: "#687784" }}>{row.host}</TableCell>
-                <TableCell sx={{ color: "#687784" }}>
+                <TableCell sx={{ color: "#687784", fontSize: "10px" }}>
+                  {row.host}
+                </TableCell>
+                <TableCell sx={{ color: "#687784", fontSize: "10px" }}>
                   {row.agent.length > 30
                     ? `${row.agent.slice(0, 30)}...`
                     : row.agent}
                 </TableCell>
 
-                <TableCell sx={{ color: "#E8657C" }}>
+                <TableCell sx={{ color: "#E8657C", fontSize: "10px" }}>
                   {formatIndianDate(row.createdAt)}
                 </TableCell>
               </TableRow>
